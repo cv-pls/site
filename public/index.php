060e421d888b1eb237bb7cdcd390d8b449b3f5a0
PeeHaa <info@pieterhordijk.com>
2013-01-13 17:17:03 +0100

    base setup

---
+++ public/index.php
@@ -0,0 +1,83 @@
+<?php date_default_timezone_set('Europe/Amsterdam'); ?>
+<html>
+  <head>
+    <title>cv-pls</title>
+    <link href='//fonts.googleapis.com/css?family=Ubuntu+Condensed' rel='stylesheet' type='text/css'>
+    <link href='//fonts.googleapis.com/css?family=PT+Sans' rel='stylesheet' type='text/css'>
+    <link href="/style/style.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+    <header>
+      <div class="content-container">
+        <h1><a href="/">cv-pls</a></h1>
+        <nav>
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/changelog">Changelog</a></li>
+            <li><a href="/source">Source</a>
+              <ul>
+                <li><a href="https://github.com/cv-pls/cv-pls" target="_blank">Base</a></li>
+                <li><a href="https://github.com/cv-pls/chrome-cv-pls" target="_blank">Chrome</a></li>
+                <li><a href="https://github.com/cv-pls/ff-cv-pls target="_blank">Firefox</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Downloads</a>
+              <ul>
+                <li><a href="">Chrome v0.20.0 (stable)</a></li>
+                <li><a href="">Firefox v0.20.0 (stable)</a></li>
+              </ul>
+            </li>
+            <li><a href="https://github.com/cv-pls/cv-pls/issues?state=open" target="_blank">Issuetracker</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    <?php /*
+    <div id="home-slider">
+      <div class="viewport">
+        <ul>
+          <li><img src="/img/slider/image1.png" alt="image 1"></li>
+          <li><img src="/img/slider/image2.png" alt="image 2"></li>
+        </ul>
+      </div>
+    </div>
+    */ ?>
+    <div id="content">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut adipiscing vestibulum neque, sit amet commodo felis auctor ut. Maecenas nisl enim, euismod vel blandit ac, viverra id justo. Curabitur hendrerit justo quis enim egestas bibendum. Proin ultrices semper dapibus. Nulla et eros at odio pellentesque hendrerit id elementum sapien. Etiam tortor nunc, volutpat nec laoreet id, vehicula vitae nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed sed enim erat.</p>
+      <p>Ut venenatis sem at massa semper tempus. Sed lectus mauris, egestas nec volutpat malesuada, pharetra at sem. In hac habitasse platea dictumst. Praesent mollis aliquet dui, eget vulputate ligula tempor sit amet. Proin ut justo dui. Nulla facilisi. Proin nibh nulla, auctor nec congue id, molestie mollis dolor. In ac nulla nulla, ac dignissim augue.</p>
+      <p>Quisque cursus nibh ut urna consectetur sit amet sagittis sem ultrices. Donec vitae eros vel lacus tristique consectetur. Integer vel dolor mauris, id auctor enim. In hac habitasse platea dictumst. Sed dolor mi, pellentesque ut dapibus eu, faucibus ut sapien. Donec vehicula ante nec libero vehicula vel hendrerit nunc elementum. Praesent bibendum magna nunc. Cras nisi enim, vulputate ac semper a, imperdiet sit amet ante. Mauris lobortis, lacus a malesuada lobortis, magna lacus pharetra nisl, ac pharetra nunc mauris ut nulla.</p>
+      <p>Sed mauris urna, congue at semper interdum, porta non nunc. Nam sed neque et neque vulputate aliquet quis ut diam. Sed aliquet pulvinar eros, ac bibendum tortor laoreet eu. Ut imperdiet sem ac quam tristique venenatis. Nam venenatis, felis id mollis interdum, odio nibh sollicitudin ligula, sit amet condimentum diam eros sed purus. Donec varius sodales velit, sed placerat libero pretium eu. Vivamus ligula eros, imperdiet sit amet volutpat sed, vulputate id erat. Etiam imperdiet nulla placerat erat mollis suscipit. Aliquam gravida varius urna, ac lacinia neque imperdiet sit amet. Etiam lacus orci, blandit id ullamcorper vitae, porta et turpis. Mauris blandit massa sed urna pharetra eget tempus ligula sollicitudin.</p>
+      <p>Sed eleifend mi pellentesque dolor faucibus tincidunt. Sed et ante in erat dictum luctus eget ac libero. Curabitur eu arcu nisl. Aliquam semper laoreet lacinia. Vestibulum aliquet sapien vel tortor lobortis non congue nunc dignissim. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed malesuada libero quis tellus blandit pretium. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+    </div>
+    <footer>
+      <ul class="info-and-links">
+        <li class="mission-statement">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut adipiscing vestibulum neque, sit amet commodo felis auctor ut. Maecenas nisl enim, euismod vel blandit ac, viverra id justo. Curabitur hendrerit justo quis enim egestas bibendum. Proin ultrices semper dapibus. Nulla et eros at odio pellentesque hendrerit id elementum sapien. Etiam tortor nunc, volutpat nec laoreet id, vehicula vitae nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed sed enim erat.</li>
+        <li class="backlog">
+          <ul>
+            <li class="title"><h3>Backlog</h3></li>
+            <li class="cv"><a href="#">Stupid question 1</a></li>
+            <li class="cv"><a href="#">Stupid question 2 with long stupid title yay</a></li>
+            <li class="cv"><a href="#">Stupid question 3 with delv</a></li>
+            <li class="more"><a href="#">more</a><li>
+          </ul>
+        </li>
+        <li class="contributors">
+          <ul>
+            <li class="title"><h3>Contributors</h3></li>
+            <li><a href="https://github.com/DaveRandom" target="_blank">Chris Wright</a></li>
+            <li><a href="https://github.com/hakre" target="_blank">Hakre</a></li>
+            <li><a href="https://github.com/PeeHaa" target="_blank">Pieter Hordijk</a></li>
+          </ul>
+        </li>
+      </ul>
+      <div class="content-container">
+        <ul class="terms-and-conditions">
+          <li>Copyright &copy; <?php echo date('Y'); ?></li>
+          <li>All right reserved</li>
+          <li><a href="">Privacy</a></li>
+          <li><a href="">Something</a></li>
+        </ul>
+      </div>
+    </footer>
+  </body>
+</html>
